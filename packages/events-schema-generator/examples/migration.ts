0bd22c89989fbf4f53dba41e0446667ad9fd06af
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-04-21 22:16:32 +0530

    feat: complete cron

--- packages/events-schema-generator/examples/migration.ts
+++ packages/events-schema-generator/examples/migration.ts
@@ -4,6 +4,7 @@ import { Contract, providers } from 'ethers';
 import { resolve } from 'path';
 import { get_latest_block } from '../../evm-helpers/src';
 import { readFileSync } from 'fs';
+import { randomUUID } from 'crypto';
 
 const prisma = new PrismaClient();
 
@@ -100,74 +101,112 @@ export const fetch_transactions_for_contract = async (contract: {
 
   const provider = get_provider(contract.chainId);
 
-  let latestBlock = await get_latest_block(provider);
-
-  const contract_data = await prisma.contract_pm.findFirstOrThrow({
+  const contract_pm = await prisma.contract_pm.findFirstOrThrow({
     where: {
       address: contract.address,
       chainId: contract.chainId,
     },
   });
 
-  const indexedTillBlock = contract_data.indexedTillBlock;
-
-  if (latestBlock - indexedTillBlock > 200) {
-    latestBlock = indexedTillBlock + 200;
-  }
+  const indexedTillBlock = contract_pm.indexedTillBlock;
+  const latestBlock = await get_latest_block(provider);
 
   const contract_instance = new Contract(
     contract.address,
     JSON.parse(readFileSync(contract.abiPath).toString()).abi,
     provider
   );
+  let currentIndexTill: number = indexedTillBlock;
+  console.log({ currentIndexTill, latestBlock });
 
-  for (let index = 0; index < contract_events.length; index++) {
-    const event_changed_name = contract_events[index];
+  while (currentIndexTill < latestBlock) {
+    if (latestBlock - currentIndexTill > 200) {
+      currentIndexTill = currentIndexTill + 200;
+    } else {
+      currentIndexTill = latestBlock;
+    }
+    for (let index = 0; index < contract_events.length; index++) {
+      const event_changed_name = contract_events[index];
 
-    const event_name = get_event_name(event_changed_name.trim(), contract.name);
-    const event = Object.values(contract_instance.interface.events).filter(
-      (e) => e.name === event_name
-    )[0]!;
-    const queryData = await contract_instance.queryFilter(
-      contract_instance.filters[event_name](),
-      indexedTillBlock,
-      latestBlock
-    );
-    const to_save_args: {
-      blockData: {
-        blockNumber: number;
-        transactionHash: string;
-        logIndex: number;
-      };
-      data: any;
-    }[] = [];
-    for (let index = 0; index < queryData.length; index++) {
-      const query = queryData[index];
-
-      const arg_keys = event.inputs.map((e) => e.name);
-
-      let arg_map = {};
-
-      for (let index = 0; index < arg_keys.length; index++) {
-        const arg_key = arg_keys[index];
-        const arg = query.args?.[arg_key];
-        const data = arg?.toString();
-        arg_map = { ...arg_map, [`A_${arg_key}`]: data };
-      }
-
-      to_save_args.push({
-        data: arg_map,
+      const event_name = get_event_name(
+        event_changed_name.trim(),
+        contract.name
+      );
+      const event = Object.values(contract_instance.interface.events).filter(
+        (e) => e.name === event_name
+      )[0]!;
+      const queryData = await contract_instance.queryFilter(
+        contract_instance.filters[event_name](),
+        indexedTillBlock,
+        currentIndexTill
+      );
+      const to_save_args: {
         blockData: {
-          blockNumber: query.blockNumber,
-          transactionHash: query.transactionHash,
-          logIndex: query.logIndex,
-        },
+          block: number;
+          transactionHash: string;
+          logIndex: number;
+        };
+        data: any;
+      }[] = [];
+      for (let index = 0; index < queryData.length; index++) {
+        const query = queryData[index];
+
+        const arg_keys = event.inputs.map((e) => e.name);
+
+        let arg_map = {};
+
+        for (let index = 0; index < arg_keys.length; index++) {
+          const arg_key = arg_keys[index];
+          const arg = query.args?.[arg_key];
+          const data = arg?.toString();
+          arg_map = { ...arg_map, [`A_${arg_key}`]: data };
+        }
+
+        to_save_args.push({
+          data: arg_map,
+          blockData: {
+            block: query.blockNumber,
+            transactionHash: query.transactionHash,
+            logIndex: query.logIndex,
+          },
+        });
+      }
+      const entry = to_save_args.map((e) => {
+        const uuid = randomUUID() as string;
+        return {
+          id: uuid,
+          event_entry: { id: uuid, ...e.blockData, ContractId: contract_pm.id },
+          data: { ...e.data, eventId: uuid },
+        };
       });
 
-      break;
+      console.log(
+        `Found events for ${event_changed_name}: ${queryData.length}`
+      );
+      const event_entry = entry.map((e) => e.event_entry);
+
+      await prisma.event_pm.createMany({
+        data: event_entry,
+      });
+
+      const event_entry_data = entry.map((e) => e.data);
+
+      await prisma[event_changed_name].createMany({
+        data: event_entry_data,
+      });
+
+      await prisma.contract_pm.update({
+        where: {
+          id: contract_pm.id,
+        },
+        data: {
+          indexedTillBlock: currentIndexTill,
+        },
+      });
     }
-    //
   }
+
+  setTimeout(() => fetch_transactions_for_contract(contract), 10_000);
 };
 
 const migrate = async () => {
