03ae8f2ee17fffeda91878e5982eb2d4da6df7fa
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-04-21 20:04:03 +0530

    feat: updated migration code

--- packages/events-schema-generator/examples/migration.ts
+++ packages/events-schema-generator/examples/migration.ts
@@ -1,6 +1,43 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
+const contracts_arr = [
+  {
+    chainId: 123,
+    initBlock: 0,
+    name: 'Ownable',
+    address: '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+    transactionHash:
+      '0xaf182a1de82819838ebce3104fdd93e87c3ad0c946c9e4844faf16f17cea15cf',
+  },
+];
+
 const add_contracts = async () => {
-  const contracts = await prisma.contract.count();
+  const contracts = await prisma.contract_pm.count();
+
+  if (contracts < contracts_arr.length) {
+    const pending_contracts = await Promise.all(
+      contracts_arr
+        .map((contract) => ({
+          ...contract,
+          indexedTillBlock: contract.initBlock,
+        }))
+        .filter(async (contract) => {
+          const find = await prisma.contract_pm.findFirst({
+            where: {
+              address: contract.address,
+              chainId: contract.chainId,
+            },
+          });
+
+          if (find === null) {
+            return true;
+          }
+          return false;
+        })
+    );
+    await prisma.contract_pm.createMany({
+      data: await Promise.all(pending_contracts),
+    });
+  }
 };
