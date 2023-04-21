e49f8c0cb87562cb02825f525d105680225d46bf
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-04-21 21:19:44 +0530

    fix: initial events

--- packages/events-schema-generator/examples/migration.ts
+++ packages/events-schema-generator/examples/migration.ts
@@ -1,12 +1,33 @@
 import { Contract_pm, PrismaClient } from '@prisma/client';
 import { execSync } from 'child_process';
+import { Contract, providers } from 'ethers';
 import { resolve } from 'path';
+import { get_latest_block } from '../../evm-helpers/src';
+import { readFileSync } from 'fs';
 const prisma = new PrismaClient();
 
-const events = ['E_Approval_Avalanche', 'E_Transfer_Avalanche'];
+const events = [
+  'e_Approval_Avalanche',
+  'e_Transfer_Avalanche',
+  'e_OrderFilledRFQ_RandomUniswap',
+  'e_OwnershipTransferred_RandomUniswap',
+  'e_Swapped_RandomUniswap',
+];
 
-const get_rpc = (chainId: number | string) => {
-  return process.env[`RPC_${chainId}_CHAIN`];
+const get_event_name = (event_name: string, contract_name: string) => {
+  return event_name.slice(2, event_name.length - contract_name.length - 1);
+};
+
+const map = new Map<number, providers.JsonRpcProvider>();
+
+const get_provider = (chainId: number | string) => {
+  let provider = map.get(parseInt(chainId.toString()));
+  if (provider) {
+    return provider;
+  }
+  provider = new providers.JsonRpcProvider(process.env[`RPC_${chainId}_CHAIN`]);
+  map.set(parseInt(chainId.toString()), provider);
+  return provider;
 };
 
 const contracts_arr = [
@@ -18,6 +39,15 @@ const contracts_arr = [
     transactionHash:
       '0xa6527d280cc8001c6417bafadf4ced4a3d619b6d7b968ed5d2792723d28a4f6a',
     abiPath: resolve(__dirname, 'Avalanche.json'),
+  },
+  {
+    chainId: 137,
+    initBlock: 41790805,
+    address: '0x1111111254fb6c44bac0bed2854e76f90643097d',
+    transactionHash:
+      '0x3392761ef9a99a3b3b2b6c4eecd047ec82c602fbdea9da6ed2e1cd4bf87603d7',
+    name: 'RandomUniswap',
+    abiPath: resolve(__dirname, 'RandomUniswap.json'),
   },
 ];
 
@@ -57,21 +87,66 @@ const add_contracts = async () => {
   return [];
 };
 
-const fetch_transactions_for_contract = async (contract: { name: string }) => {
+export const fetch_transactions_for_contract = async (contract: {
+  name: string;
+  address: string;
+  chainId: number;
+  abiPath: string;
+}) => {
   const contract_events = events.filter(
-    (e) => e.startsWith(`E_`) && e.endsWith(`_${contract.name}`)
+    (e) => e.startsWith(`e_`) && e.endsWith(`_${contract.name}`)
   );
 
-  console.log(contract_events);
+  const provider = get_provider(contract.chainId);
+
+  let latestBlock = await get_latest_block(provider);
+
+  const contract_data = await prisma.contract_pm.findFirstOrThrow({
+    where: {
+      address: contract.address,
+      chainId: contract.chainId,
+    },
+  });
+
+  const indexedTillBlock = contract_data.indexedTillBlock;
+
+  if (latestBlock - indexedTillBlock > 200) {
+    latestBlock = indexedTillBlock + 200;
+  }
+
+  const contract_instance = new Contract(
+    contract.address,
+    JSON.parse(readFileSync(contract.abiPath).toString()).abi,
+    provider
+  );
+
+  for (let index = 0; index < contract_events.length; index++) {
+    const event = contract_events[index];
+
+    const event_name = get_event_name(event.trim(), contract.name);
+    console.log(event_name);
+    const queryData = await contract_instance.queryFilter(
+      contract_instance.filters[event_name](),
+      indexedTillBlock,
+      latestBlock
+    );
+    for (let index = 0; index < queryData.length; index++) {
+      const query = queryData[index];
+      console.log(query);
+      break;
+    }
+  }
 };
 
 const migrate = async () => {
   const added_contracts = await add_contracts();
 
-  if (added_contracts[0]) {
-    await fetch_transactions_for_contract(added_contracts[0]);
+  for (let index = 0; index < contracts_arr.length; index++) {
+    const contract = contracts_arr[index];
+    await fetch_transactions_for_contract(contract);
   }
+
   return added_contracts;
 };
 
-migrate().then(console.log).catch(console.error);
+migrate().catch(console.error);
