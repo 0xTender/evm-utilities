b62e08f91d84e6bad8a16f387f9242d0b14195af
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2023-04-22 16:28:43 +0530

    fix: migration

--- packages/heisenberg-cli/src/lib/heisenberg.ts
+++ packages/heisenberg-cli/src/lib/heisenberg.ts
@@ -1,5 +1,9 @@
 #!/usr/bin/env ts-node
-import { get_contract } from '@0xtender/evm-helpers';
+import {
+  get_contract,
+  get_contract_metadata,
+  get_json_rpc_provider,
+} from '@0xtender/evm-helpers';
 import { generate_events_schema } from '@0xtender/events-schema-generator';
 import { Command } from 'commander';
 
@@ -8,6 +12,7 @@ import { dirname, join, resolve } from 'path';
 
 import { z } from 'zod';
 import { providers } from 'ethers';
+import { generate_migration } from '../../../events-schema-generator/src/generate_migration';
 
 const program = new Command();
 
@@ -42,65 +47,7 @@ program
 
       contracts_json = resolve(contracts_json);
 
-      const contracts_arr = JSON.parse(readFileSync(contracts_json).toString());
-
-      const schema = z.array(
-        z.object({
-          path: z.string(),
-          name: z.string(),
-          rpc: z.string(),
-        })
-      );
-
-      const contracts_parsed = schema.parse(contracts_arr).map((contract) => {
-        // try absolute path first
-        let contract_path = resolve(contract.path);
-
-        if (existsSync(resolve(contract.path))) {
-          return {
-            ...contract,
-            path: contract_path,
-          };
-        }
-
-        // try relative path to contracts.json
-        contract_path = join(dirname(contracts_json), contract.path);
-
-        if (existsSync(contract_path)) {
-          return {
-            ...contract,
-            path: contract_path,
-          };
-        }
-
-        throw new Error(`Contract ${contract.path} not found`);
-      });
-
-      const contracts = [];
-
-      for (let index = 0; index < contracts_parsed.length; index++) {
-        const contract = contracts_parsed[index];
-
-        let provider = providers.getDefaultProvider(contract.rpc);
-
-        // provider = get_json_rpc_provider(contract.rpc);
-
-        const data: {
-          address: string;
-          abi: any;
-          transactionHash: string;
-        } = JSON.parse(readFileSync(contract.path).toString());
-        contracts.push({
-          ...contract,
-          data: {
-            ...data,
-            provider,
-            instance: get_contract(data.address, data.abi),
-            name: contract.name,
-            abiPath: contract.path,
-          },
-        });
-      }
+      const contracts = get_contracts(contracts_json);
 
       const rendered = await generate_events_schema(
         contracts.map((c) => c.data),
@@ -108,9 +55,93 @@ program
         template
       );
 
-      // console.log(options, rendered);
       console.log(`Writing to ${resolve(options.outputFile)}`);
       writeFileSync(options.outputFile, rendered);
     }
   );
+
+program
+  .command('generate-migration')
+  .description('Generate migration script')
+  .argument('<contracts.json>', 'contracts to generate migration for')
+  .option(
+    '-i, --input-file <input_file>',
+    'base input file for the migration (optional)'
+  )
+  .requiredOption(
+    '-o, --output-file <output_file>',
+    'output file for the migration'
+  )
+  .action(async (contracts_json: string, options: { outputFile: string }) => {
+    const contracts = get_contracts(contracts_json).map((c) => ({
+      ...c,
+      ...c.data,
+    }));
+
+    const migration_script = await generate_migration(contracts, 'postgres');
+    console.log(`Writing to ${resolve(options.outputFile)}`);
+    writeFileSync(options.outputFile, migration_script);
+  });
+
 program.parse();
+
+function get_contracts(contracts_json: string) {
+  const contracts_arr = JSON.parse(readFileSync(contracts_json).toString());
+
+  const schema = z.array(
+    z.object({
+      path: z.string(),
+      name: z.string(),
+      rpc: z.string(),
+    })
+  );
+
+  const contracts_parsed = schema.parse(contracts_arr).map((contract) => {
+    // try absolute path first
+    let contract_path = resolve(contract.path);
+
+    if (existsSync(resolve(contract.path))) {
+      return {
+        ...contract,
+        path: contract_path,
+      };
+    }
+
+    // try relative path to contracts.json
+    contract_path = join(dirname(contracts_json), contract.path);
+
+    if (existsSync(contract_path)) {
+      return {
+        ...contract,
+        path: contract_path,
+      };
+    }
+
+    throw new Error(`Contract ${contract.path} not found`);
+  });
+
+  const contracts = [];
+
+  for (let index = 0; index < contracts_parsed.length; index++) {
+    const contract = contracts_parsed[index];
+
+    const provider = get_json_rpc_provider(contract.rpc);
+    const data: {
+      address: string;
+      abi: any;
+      transactionHash: string;
+    } = JSON.parse(readFileSync(contract.path).toString());
+    const instance = get_contract(data.address, data.abi, provider);
+    contracts.push({
+      ...contract,
+      data: {
+        ...data,
+        provider,
+        instance: instance,
+        name: contract.name,
+        abiPath: contract.path,
+      },
+    });
+  }
+  return contracts;
+}
